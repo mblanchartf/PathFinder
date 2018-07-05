f968647ed999fd67e4fecaf09782c1360adcd6a7
amquack05 <marcblanchart@gmail.com>
2018-07-05 19:55:14 +0200

    first commit

---
+++ keras_path_model.py
@@ -0,0 +1,137 @@
+import os
+import cv2
+import numpy as np
+import argparse
+import time
+from sklearn.utils import shuffle
+from sklearn.cross_validation import train_test_split
+from keras.utils import np_utils
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation, Flatten
+from keras.layers.convolutional import MaxPooling2D, Conv2D
+# from keras.optimizers import SGD, RMSprop, adam
+from keras import backend as K
+K.set_image_dim_ordering('th')
+
+# Arguments parser
+parser = argparse.ArgumentParser(
+        description='Generate model from a dataset using Keras')
+optional = parser._action_groups.pop()
+required = parser.add_argument_group('required arguments')
+required.add_argument('-dpath', '--dataset_path', required=True, type=str,
+                      help='Dataset folder')
+parser._action_groups.append(optional)
+
+# Prepare general data
+args = parser.parse_args()
+data_path = args.dataset_path
+
+
+data_dir_list = os.listdir(data_path)
+img_list = os.listdir(data_path)
+
+rows = 300
+cols = 300
+channels = 1
+n_epoch = 20
+
+img_data_list = []
+
+for dataset in data_dir_list:
+    img_list = os.listdir(data_path + '/' + dataset)
+    print('Loaded the images of dataset-' + '{}\n'.format(dataset))
+    for img in img_list:
+        input_img = cv2.imread(data_path + '/' + dataset + '/' + img)
+        input_img = cv2.cvtColor(input_img, cv2.COLOR_BGR2GRAY)
+        img_data_list.append(input_img)
+
+img_data = np.array(img_data_list)
+img_data = img_data.astype('float32')
+img_data /= 255
+print(img_data.shape)
+
+if channels == 1:
+    if K.image_dim_ordering() == 'th':
+        img_data = np.expand_dims(img_data, axis=1)
+        print(img_data.shape)
+    else:
+        img_data = np.expand_dims(img_data, axis=4)
+        print(img_data.shape)
+else:
+    if K.image_dim_ordering() == 'th':
+        img_data = np.rollaxis(img_data, 3, 1)
+        print(img_data.shape)
+
+# Define the number of classes to identify: paths, non_paths
+num_classes = 2
+
+num_of_samples = img_data.shape[0]
+labels = np.ones((num_of_samples, ), dtype='int64')
+
+labels[0:3320] = 0
+labels[3321:] = 1
+
+# Convert class labels to on-hot encoding
+Y = np_utils.to_categorical(labels, num_classes)
+
+# Shuffle the dataset
+x, y = shuffle(img_data, Y, random_state=2)
+# Split the dataset
+X_train, X_test, y_train, y_test = train_test_split(x, y,
+                                                    test_size=0.2, 
+                                                    random_state=2)
+
+# Defining the model
+input_shape = img_data[0].shape
+
+model = Sequential()
+
+model.add(Conv2D(32, 3, 3, border_mode='same', input_shape=input_shape))
+model.add(Activation('relu'))
+model.add(Conv2D(32, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Conv2D(64, 3, 3))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.5))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='rmsprop',
+              metrics=["accuracy"])
+
+# Viewing model_configuration
+'''
+model.summary()
+model.get_config()
+model.layers[0].get_config()
+model.layers[0].input_shape
+model.layers[0].output_shape
+model.layers[0].get_weights()
+np.shape(model.layers[0].get_weights()[0])
+model.layers[0].trainable'''
+
+# Training
+hist = model.fit(X_train, y_train,
+                 batch_size=16,
+                 nb_epoch=n_epoch, verbose=1,
+                 validation_data=(X_test, y_test))
+
+score = model.evaluate(X_test, y_test, verbose=0)
+print('Test accuarcy: {:0.2f}%'.format(score[1] * 100))
+
+timestr = time.strftime("%Y%m%d%H%M%S_")
+
+model.save(data_path + timestr + 'model_keras_path.hdf5')
+
+print('Model saved at ' + data_path + ' named '
+      + timestr + 'model_keras_path.hdf5')
